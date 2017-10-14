dd0d071387dedd9aa8d20c0089170e7443b89358
StriveStruggle <434845113@qq.com>
2017-10-14 14:51:40 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/LearningSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringCloudApplication.class, args);
+	}
+}
